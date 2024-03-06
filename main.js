92adfcd6b9e32744171efa660e827146c9e0a407
Adly <adly.abdullah@mmob.com>
2024-03-06 17:01:02 +0800

    Don't include htmx twice

--- main.js
+++ main.js
@@ -218,7 +218,7 @@ function init(app, componentsDir, options) {
 	/**
 	 * @type {SrcUrl[]}
 	 */
-	let jsToInclude = [htmxToInclude];
+	let jsToInclude = [];
 
 	if (options) {
 		if (!options.js) {
